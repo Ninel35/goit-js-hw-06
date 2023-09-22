5792f6c6af4d92c0c030c716e774253673f9ec79
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-09-22 18:00:47 +0200

    task4

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const decrement = document.querySelector('button[data-action="decrement"]');
+const increment = document.querySelector('button[data-action="increment"]');
+const counter = document.querySelector("#value");
+
+let counterValue = 0;
+
+function handlerD() {
+    counterValue -= 1;
+    counter.textContent = counterValue;
+    
+}
+
+decrement.addEventListener("click", handlerD);
+
+function handlerI() {
+    counterValue += 1;
+    counter.textContent = counterValue;
+   
+}
+
+increment.addEventListener("click", handlerI);
