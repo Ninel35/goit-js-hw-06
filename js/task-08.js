b42f953dd25d0469e82a94f356e52d6320755c1d
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-09-24 10:41:16 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleForm);
+
+function handleForm(evt) {
+    evt.preventDefault();
+
+    const logData = {
+        email: evt.currentTarget.elements.email.value,
+        password: evt.currentTarget.elements.password.value,
+    }
+    if (logData.email === "" || logData.password === "") alert("All fields required");
+    console.log(logData);
+    evt.currentTarget.reset();
+}
