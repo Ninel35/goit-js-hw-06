250b2a3c75906031c9777dc7b38b0a0c10e824c5
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-09-24 13:43:52 +0200

    task9

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const inpt = document.querySelector("#name-input");
 const textSpan = document.querySelector("#name-output");
 
 function handleInp(event) {
-    textSpan.textContent = event.currentTarget.value;
-    if (event.currentTarget.value === "") textSpan.textContent = "Anonymuos";
+    textSpan.textContent = event.currentTarget.value || "Anonymuos";
 }
 inpt.addEventListener("input", handleInp);
