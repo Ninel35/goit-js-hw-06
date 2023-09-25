edcfaeed89769ba4c980c2bbd62ace0fc5a91d6a
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-09-25 17:42:57 +0200

    tasknew7

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const inp = document.querySelector("#font-size-control");
 const span = document.querySelector("#text");
 
 inp.addEventListener("input", handleSpan);
-
+span.style.fontSize = inp.value+"px";
 function handleSpan(event) {
     span.style.fontSize = event.currentTarget.value +"px";
 }
